3f38e5d0fd4b37b8440e4d7631bae0c1f6679308
Richard Riedel <richardalexriedel@yahoo.com>
2018-04-09 12:33:48 -0400

    Added early exit checks for stuff that fails

--- canvas.sh
+++ canvas.sh
@@ -5,6 +5,7 @@
 # on canvas, with thier specified names. 
 # This script requires 
 
+# Gets the name of the assignment from the cmd line arguments.
 ASSIGNMENT=$1
 export HOST="https://utk.instructure.com"
 export API_VERS="api/v1"
@@ -25,15 +26,34 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+if [ ! -f "$SCRIPT_PATH/token" ]; then
+    printf "token needed for use with Canvas API\nAdd a token to this path $SCRIPT_PATH\n";
+    exit 2
+fi
+
 export TOKEN=$(<$SCRIPT_PATH/token)
 
 # Requests courses for user.
 COURSES_REQUEST=$(curl $HOST/$API_VERS/$COURSES_PATH/ \
                    -H "Authorization: Bearer "$TOKEN \
                    2> /dev/null)
+RV=$?
+if [ ! $RV ]; then
+    printf "Course request failed.\nExiting....\n"
+    exit $RV
+fi
 
 # Extracts the Course ID from the course code.
-export ID=$(python $SCRIPT_PATH/extract_course_id.py $COURSE_CODE <<< """$COURSES_REQUEST""")
+ID=$(python $SCRIPT_PATH/extract_course_id.py $COURSE_CODE <<< """$COURSES_REQUEST""")
+
+RV=$?
+if [ "$RV" != "0" ]; then
+    printf "Extracting Course ID Failed.\nExiting....\n"
+    exit $RV
+fi
+
+export ID
+
 
 # Gets a json request of all assignments offered on a course. (This needs to be pagenated)
 ASSIGNMENTS_REQUEST=$(curl $HOST/$API_VERS/$COURSES_PATH/$ID/$ASSIGNMENTS_PATH/ \
@@ -41,11 +61,35 @@ ASSIGNMENTS_REQUEST=$(curl $HOST/$API_VERS/$COURSES_PATH/$ID/$ASSIGNMENTS_PATH/ 
                        -H "Authorization: Bearer "$TOKEN \
                        2> /dev/null)
 
+RV=$?
+if [ "$RV" != "0" ]; then
+    printf "Assignment Request Failed.\nExiting....\n"
+    exit $RV
+fi
 
 # Extracts the assignment id number from the assignment string passed
-export ASSIGNMENT_ID=$(python $SCRIPT_PATH/extract_assignment_id.py "$ASSIGNMENT" <<< """$ASSIGNMENTS_REQUEST""")
+ASSIGNMENT_ID=$(python $SCRIPT_PATH/extract_assignment_id.py "$ASSIGNMENT" <<< """$ASSIGNMENTS_REQUEST""")
 
+RV=$?
+if [ "$RV" != "0" ]; then
+    printf "Assignment ID Extraction Failed.\nExiting....\n"
+    exit $RV
+fi
+
+export ASSIGNMENT_ID
 # Downloads all the submissions of the specified ASSIGNMENT_ID environment variable.
 DOWNLOAD_LINKS=$(python $SCRIPT_PATH/download_submissions.py)
 
+RV=$?
+if [ "$RV" != "0" ]; then
+    printf "Downloading Submissions Failed.\nExiting....\n"
+    exit $RV
+fi
+
 (python $SCRIPT_PATH/split_into_groups.py)
+
+RV=$?
+if [ "$RV" != "0" ]; then
+    printf "Splitting into Groups Failed.\nExiting....\n"
+    exit $RV
+fi
